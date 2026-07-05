4fd8108a1bf335f2fe3c481496b6807f54615d83
Sonu Soni <1by23cb053@bmsit.in>
2026-07-05 23:48:18 +0530

    Time: 17 ms (43.01%), Space: 108.1 MB (8.51%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,14 +1,12 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-
-        HashMap <Integer, Integer> map = new HashMap<>();
+        
+        HashSet <Integer> set = new HashSet<>();
 
         for(int x : nums){
-            map.put(x, map.getOrDefault(x, 0) +1);
-        }
-
-        for(int x : map.keySet()){
-            if(map.get(x) >1)   return true;
+            if(set.contains(x))
+                return true;
+            set.add(x);
         }
         return false;
     }
