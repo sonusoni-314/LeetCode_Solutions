b6ddfb70da29ac986c51fce5ff4bdd2dd73ea936
Sonu Soni <1by23cb053@bmsit.in>
2026-07-12 22:54:00 +0530

    Time: 13 ms (98.9%), Space: 51.5 MB (37.03%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.java
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -4,7 +4,9 @@ class Solution {
         Arrays.sort(g);
         Arrays.sort(s);
 
-        int i = 0, j = 0, count = 0;
+        int i = 0;
+        int j = 0;
+        int count = 0;
 
         while(i<g.length && j<s.length){
 
