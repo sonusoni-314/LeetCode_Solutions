1b449ef8ce189c800be7c745340d91f2f3c7932c
Sonu Soni <1by23cb053@bmsit.in>
2026-07-12 06:30:37 +0530

    Time: 32 ms (51.36%), Space: 75.5 MB (53.05%) - LeetHub

--- 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.java
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.java
@@ -1,25 +1,16 @@
 class Solution {
-    public int[] arrayRankTransform(int[] arr) {
-        
-        int[] rank = arr.clone();
-        Arrays.sort(rank);
+    public int[] arrayRankTransform(int[] arr) {  
+        int[] nums = arr.clone();
+        Arrays.sort(nums);
         HashMap <Integer, Integer> map = new HashMap<>();
-
-        int count = 1;
-
-        for(int i=0; i<rank.length; i++){
-            if(!map.containsKey( rank[i] )){
-                map.put(rank[i], count++);
-            }
-            
+        int rank = 1;
+        for(int x : nums){
+            if(!map.containsKey(x))
+                map.put(x, rank++);
         }
-
         int[] ans = new int[arr.length];
-
-        for(int i=0; i<arr.length; i++){
+        for(int i=0; i<arr.length; i++)
             ans[i] = map.get(arr[i]);
-        }
-
         return ans;
     }
 }
