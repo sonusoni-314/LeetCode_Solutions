7e65e1fea606abfe94fbc23d897fa1a1f882da4c
Sonu Soni <1by23cb053@bmsit.in>
2026-07-12 22:52:50 +0530

    Time: 6 ms (99.94%), Space: 51.1 MB (93.12%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.java
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -1,24 +1,32 @@
 class Solution {
     public int findContentChildren(int[] g, int[] s) {
+        Thread t1 = new Thread(() -> {
+            Arrays.sort(g);
+        });
+        Thread t2 = new Thread(() -> {
+            Arrays.sort(s);
+        });
 
-        Arrays.sort(g);
-        Arrays.sort(s);
+        t1.start();
+        t2.start();
 
-        int i = 0;
-        int j = 0;
-        int count = 0;
-
-        while(i<g.length && j<s.length){
-
-            if(s[j] >= g[i]){
-                count++;
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+        }
+        int i = 0, j = 0, c = 0;
+        while (i < g.length && j < s.length) {
+            if (s[j] >= g[i]) {
+                c++;
                 i++;
                 j++;
-            }
-            else{
+            } else {
                 j++;
             }
+
         }
-        return count;
+        return c;
+
     }
 }
