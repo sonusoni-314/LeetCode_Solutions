305da9409ab84124ca98ac9b791d73096c80f6a5
Sonu Soni <1by23cb053@bmsit.in>
2026-07-14 00:07:33 +0530

    Time: 2 ms (89.04%), Space: 47.8 MB (81.2%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -5,9 +5,10 @@ class Solution {
 
         for(int i=0; i<nums.length; i++){
 
-            if(i > max) return false;
-
-            max = Math.max(i+nums[i], max);
+            if(i > max){
+                return false;
+            }
+            max = Math.max(max, i + nums[i]);
             if(max >= nums.length-1){
                 return true;
             }
