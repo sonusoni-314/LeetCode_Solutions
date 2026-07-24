7df0f913d8c0ff18d794a2496448f5473b2fbcea
Sonu Soni <1by23cb053@bmsit.in>
2026-07-24 17:50:49 +0530

    Time: 2 ms (99.76%), Space: 48.5 MB (88.89%) - LeetHub

--- 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.java
+++ 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.java
@@ -1,30 +1,28 @@
 class Solution {
     public int maxSatisfied(int[] customers, int[] grumpy, int minutes) {
-
+        
         int sum = 0;
+        int max = 0;
         for(int i=0; i<customers.length; i++){
-            if(grumpy[i] != 1){
+            if(grumpy[i] == 0){
                 sum += customers[i];
                 customers[i] = 0;
             }
         }
-        
-        int max = 0;
 
         for(int i=0; i<minutes; i++){
-            sum += customers[i];
+            sum+= customers[i];
         }
         max = sum;
 
         int left = 0;
         int right = minutes-1;
-
         while(right < customers.length-1){
-            sum -= customers[left];
-            
+            sum-=customers[left];
+
             left++; right++;
 
-            sum += customers[right];
+            sum+=customers[right];
             max = Math.max(max, sum);
         }
         return max;
