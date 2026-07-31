f465c1c5f4b6227e7f031cea0be2fd109b3c9ec9
Sonu Soni <1by23cb053@bmsit.in>
2026-07-31 15:43:31 +0530

    Time: 1 ms (24.21%), Space: 43.1 MB (18.59%) - LeetHub

---
+++ 0709-to-lower-case/0709-to-lower-case.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String toLowerCase(String s) {
+
+        StringBuilder sb = new StringBuilder();
+
+        for(char ch : s.toCharArray()){
+
+            if(Character.isUpperCase(ch)){
+
+                char lower = Character.toLowerCase(ch);
+                sb.append(lower);
+
+            }else{
+                sb.append(ch);
+            }
+        }
+        return sb.toString();
+    }
+}
