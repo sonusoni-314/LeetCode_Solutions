51d8e23a4ccea532cce6900fdd6a7e5a51579c03
Sonu Soni <1by23cb053@bmsit.in>
2026-08-07 19:13:01 +0530

    Time: 1 ms (93.85%), Space: 45.7 MB (19.94%) - LeetHub

---
+++ 3232-find-if-digit-game-can-be-won/3232-find-if-digit-game-can-be-won.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean canAliceWin(int[] nums) {
+
+        int single = 0;
+        int doublee = 0;
+
+        for(int x : nums){
+            int digits = (int)Math.log10(x) + 1;
+            if(digits == 1){
+                single = single + x;
+            }else{
+                doublee = doublee + x;
+            }
+        }
+        if(single > doublee || doublee > single){
+            return true;
+        }
+        return false;        
+    }
+}
