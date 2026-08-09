837817fa26b30f896a1e3b4c1497429342726438
Sonu Soni <1by23cb053@bmsit.in>
2026-08-09 22:25:55 +0530

    Time: 30 ms (35.62%), Space: 46.2 MB (80.18%) - LeetHub

--- 1390-four-divisors/1390-four-divisors.java
+++ 1390-four-divisors/1390-four-divisors.java
@@ -1,36 +1,25 @@
 class Solution {
     public int sumFourDivisors(int[] nums) {
 
-        ArrayList <Integer> list = new ArrayList <>();
-
-        int x = 0, count = 0, n = 0;
+        int x = 0, count = 0, n = 0, sum = 0, divisorSum = 0;
         while(x < nums.length){
             n = nums[x];
             count = 0;
+            divisorSum = 0;
 
             for(int i=1; i*i<=n; i++){
                 if(n%i == 0){
                     count++;
-                    if(i != n/i)
+                    divisorSum += i;
+                    if(i != n/i){
                         count++;
+                        divisorSum += n/i;
+                    }
                 }
             }
             if(count == 4)
-                list.add(n);
+                sum += divisorSum;
             x++;
-        }
-        int sum = 0;
-        int k = 0;
-        while(k < list.size()){
-            int y = list.get(k);
-            for(int i=1; i*i<=y; i++){
-                if(y%i == 0){
-                    sum += i;
-                    if(i != y/i)
-                        sum += y/i;
-                }
-            }
-            k++;
         }
         return sum;
     }
