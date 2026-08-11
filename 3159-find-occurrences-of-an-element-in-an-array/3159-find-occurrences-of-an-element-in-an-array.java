ac3a68693d6868718e3e0db69a844f6eb4c9c9e3
Sonu Soni <1by23cb053@bmsit.in>
2026-08-11 18:36:23 +0530

    Time: 4 ms (96.46%), Space: 173.2 MB (41.16%) - LeetHub

--- 3159-find-occurrences-of-an-element-in-an-array/3159-find-occurrences-of-an-element-in-an-array.java
+++ 3159-find-occurrences-of-an-element-in-an-array/3159-find-occurrences-of-an-element-in-an-array.java
@@ -7,16 +7,10 @@ class Solution {
                 list.add(i);
         }
 
-        HashMap <Integer, Integer> map = new HashMap <>();
-        for(int i:nums){
-            if(i == x)
-                map.put(i, map.getOrDefault(i, 0) +1);
-        }
-
         int[] ans = new int[queries.length];
         int k = 0;
         for(int i=0; i<queries.length; i++){
-            if(map.getOrDefault(x, 0) >= queries[i]){
+            if(list.size() >= queries[i]){
                 ans[k] = list.get(queries[i] - 1);
                 k++;
             }
