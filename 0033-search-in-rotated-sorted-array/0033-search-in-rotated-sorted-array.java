62a708087e5f3ffa78ef828cdc92094c85cc9632
Sonu Soni <1by23cb053@bmsit.in>
2026-08-12 00:44:06 +0530

    Time: 0 ms (100%), Space: 44.1 MB (11.29%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,9 +1,30 @@
 class Solution {
     public int search(int[] nums, int target) {
 
-        for(int i=0; i<nums.length; i++){
-            if(nums[i] == target){
-                return i;
+        int low = 0;
+        int high = nums.length -1;
+        int mid = 0;
+
+        while(low <= high){
+            mid = low + (high - low)/2;
+
+            if(nums[mid] == target) return mid;
+
+            if(nums[low] <= nums[mid]){
+                if(nums[low] <= target && nums[mid] > target){
+                    high = mid - 1;
+                }
+                else{
+                    low = mid + 1;
+                }
+            }
+            else{
+                if(nums[high] >= target && nums[mid] < target){
+                    low = mid + 1;
+                }
+                else{
+                    high = mid - 1;
+                }
             }
         }
         return -1;
