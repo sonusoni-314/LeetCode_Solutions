d2e44373cf730bb401311245d57625325e6a3b37
Sonu Soni <1by23cb053@bmsit.in>
2026-08-11 15:29:18 +0530

    Time: 16 ms (4.78%), Space: 56.8 MB (9.03%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,11 +1,17 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-        int n=nums.length;
-        int ans=0;
-        for(int i=0;i<n;i++){
-            ans=ans^nums[i];
+
+        HashMap <Integer, Integer> map = new HashMap <>();
+
+        for(int x : nums){
+            map.put(x, map.getOrDefault(x, 0) +1);
         }
-        return ans;
-        
+
+        for(int x : map.keySet()){
+            if(map.get(x) == 1){
+                return x;
+            }
+        }
+        return -1;
     }
 }
