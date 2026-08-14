eb5b3ad4af85a83bb7a11c157a17420e5e647711
Sonu Soni <1by23cb053@bmsit.in>
2026-08-14 10:23:27 +0530

    Time: 0 ms (100%), Space: 44.9 MB (82.86%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
@@ -1,8 +1,36 @@
 class Solution {
     public boolean search(int[] nums, int target) {
 
-        for(int x:nums){
-            if(x == target) return true;
+        int mid = 0;
+        int low = 0;
+        int high = nums.length-1;
+
+        while(low <= high){
+            mid = low + (high-low)/2;
+
+            if(nums[mid] == target) return true;
+
+            if(nums[mid]==nums[low] && nums[mid]==nums[high]){
+                low += 1;
+                high -=1;
+                continue;
+            }
+
+            if(nums[mid] >= nums[low]){
+                if(nums[low] <= target && target < nums[mid]){
+                    high = mid - 1;
+                }else{
+                    low = mid + 1;
+                }
+
+            }
+            else{
+                if(nums[high] >= target && target > nums[mid]){
+                    low = mid + 1;
+                }else{
+                    high = mid -1;
+                }
+            }
         }
         return false;
     }
