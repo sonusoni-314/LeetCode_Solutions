6b3b69cc263051131e4c50dac69389fc59db5ffd
Sonu Soni <1by23cb053@bmsit.in>
2026-08-14 08:58:18 +0530

    Time: 0 ms (100%), Space: 44.9 MB (82.86%) - LeetHub

---
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean search(int[] nums, int target) {
+
+        for(int x:nums){
+            if(x == target) return true;
+        }
+        return false;
+    }
+}
