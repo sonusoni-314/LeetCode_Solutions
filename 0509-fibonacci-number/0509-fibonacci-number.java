2132bdb5a52428ecfde964bff342d98127863904
Sonu Soni <1by23cb053@bmsit.in>
2026-08-16 19:00:55 +0530

    Time: 38 ms (9.44%), Space: 46.3 MB (16.18%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,13 +1,22 @@
 class Solution {
     public int fib(int n) {
 
+        int[] dp = new int[n+1];
+        Arrays.fill(dp, -1);
+
         if(n == 0){
             return 0;
         }
+
         if(n == 1){
             return 1;
         }
 
+        if(dp[n] != -1){
+            return dp[n];
+        }
+
         return fib(n-1) + fib(n-2);
+        
     }
 }
