02db794eaffcbe60c3711fb9560d39c2df12d652
Sonu Soni <1by23cb053@bmsit.in>
2026-08-17 17:28:43 +0530

    Time: 6 ms (51.1%), Space: 45 MB (43.17%) - LeetHub

---
+++ 2320-count-number-of-ways-to-place-houses/2320-count-number-of-ways-to-place-houses.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int countHousePlacements(int n) {
+
+        int[] dp = new int[n+1];
+        dp[0] = 1;
+        dp[1] = 2;
+        for(int i=2; i<=n; i++){
+            dp[i] = (dp[i-1] + dp[i-2]) %1000000007;
+        }
+        return (int)((long)dp[n]*dp[n] % 1000000007);        
+    }
+}
