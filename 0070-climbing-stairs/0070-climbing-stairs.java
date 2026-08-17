7cbe4d43fb64d4870847e936a3d4477e56e9fe3b
Sonu Soni <1by23cb053@bmsit.in>
2026-08-17 15:00:27 +0530

    Time: 0 ms (100%), Space: 42.3 MB (17.73%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,13 +1,14 @@
 class Solution {
     public int climbStairs(int n) {
+        if(n==0 || n==1 || n==2)    return n;
+        int[] dp = new int[n+1];
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2;
 
-        int a = 1;
-        int b = 1;
-        for(int i=n-2; i>=0; i--){
-            int temp = a + b;
-            a = b;
-            b = temp;
+        for(int i=3; i<=n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
         }
-        return b;
+        return dp[n];
     }
 }
