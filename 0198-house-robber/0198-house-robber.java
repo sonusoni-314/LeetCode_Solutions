977ff086ea658f68ce9ceb9080e1487145d2096d
Sonu Soni <1by23cb053@bmsit.in>
2026-08-19 04:08:43 +0530

    Time: 0 ms (100%), Space: 42.7 MB (75.82%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,15 +1,41 @@
 class Solution {
     public int rob(int[] nums) {
+        int[] dp = new int[nums.length+1];
+        Arrays.fill(dp, -1);
+        return solve(0, nums, dp);                
+    }
 
-        int rob1 = 0;
-        int rob2 = 0;
-        int temp = 0;
+    public int solve(int i, int[] nums, int[] dp){
+        if(i >= nums.length)
+            return 0;
 
-        for(int i=0; i<nums.length; i++){
-            temp = Math.max(rob1 + nums[i], rob2);
-            rob1 = rob2;
-            rob2 = temp;
-        }
-        return rob2;        
+        if(dp[i] != -1)
+            return dp[i];
+
+        int steal = nums[i] + solve(i+2, nums, dp);
+        int skip = solve(i+1, nums, dp);
+        return dp[i] = Math.max(steal, skip);
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+        // int rob1 = 0;
+        // int rob2 = 0;
+        // int temp = 0;
+
+        // for(int i=0; i<nums.length; i++){
+        //     temp = Math.max(rob1 + nums[i], rob2);
+        //     rob1 = rob2;
+        //     rob2 = temp;
+        // }
+        // return rob2;
