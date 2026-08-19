2af87764d61e8cc45d41b76c94fc83ea8999d5e0
Sonu Soni <1by23cb053@bmsit.in>
2026-08-19 05:54:39 +0530

    Time: 0 ms (100%), Space: 42.8 MB (42.35%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -2,40 +2,33 @@ class Solution {
     public int rob(int[] nums) {
 
         int n = nums.length;
+        int[] dp1 = new int[n];
 
-        if (n == 1) {
+        if(n == 1){
             return nums[0];
         }
 
-        if(n==2){
+        if(n == 2){
             return Math.max(nums[0], nums[1]);
         }
-
-        // Case 1: Exclude last house
-        int[] dp1 = new int[n];
 
         dp1[0] = nums[0];
         dp1[1] = Math.max(nums[0], nums[1]);
 
-        for (int i = 2; i < n - 1; i++) {
-            dp1[i] = Math.max(dp1[i - 1], nums[i] + dp1[i - 2]);
+        for(int i=2; i<n-1; i++){
+            dp1[i] = Math.max(dp1[i-1], nums[i] + dp1[i-2]);
         }
 
-        int x = dp1[n - 2];
+        int x = dp1[n-2];
 
-
-        // Case 2: Exclude first house
         int[] dp2 = new int[n];
-
         dp2[1] = nums[1];
         dp2[2] = Math.max(nums[1], nums[2]);
 
-        for (int i = 3; i < n; i++) {
-            dp2[i] = Math.max(dp2[i - 1], nums[i] + dp2[i - 2]);
+        for(int i=3; i<n; i++){
+            dp2[i] = Math.max(dp2[i-1], dp2[i-2] + nums[i]);
         }
 
-        int y = dp2[n - 1];
-
-        return Math.max(x, y);
+        return Math.max(x, dp2[n-1]);        
     }
 }
