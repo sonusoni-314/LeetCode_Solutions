558c1a29d5bb28fb28d12e4be2413f7e68236901
Sonu Soni <1by23cb053@bmsit.in>
2026-08-22 09:06:49 +0530

    Time: 0 ms (100%), Space: 42.1 MB (91.23%) - LeetHub

---
+++ 3622-check-divisibility-by-digit-sum-and-product/3622-check-divisibility-by-digit-sum-and-product.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean checkDivisibility(int n) {
+
+        int og = n;
+        int sum = 0;
+        int product = 1;
+
+        while(n != 0){
+            int digit = n%10;
+            sum += digit;
+            product *= digit;
+            n /=10;
+        }
+
+        int val = sum + product;
+        if(og%val == 0)
+            return true;
+
+        return false;        
+    }
+}
