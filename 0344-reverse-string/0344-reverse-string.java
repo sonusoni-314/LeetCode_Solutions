0333384bb9d8520af2ec4a57d1c7719e6af80510
Sonu Soni <1by23cb053@bmsit.in>
2026-08-22 23:50:43 +0530

    Time: 0 ms (100%), Space: 48.5 MB (10.69%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.java
+++ 0344-reverse-string/0344-reverse-string.java
@@ -1,15 +1,16 @@
 class Solution {
     public void reverseString(char[] s) {
 
-        int i = 0;
-        int j = s.length-1;
+        int right = s.length-1;
+        int left = 0;
 
-        while(i < j){
-            char temp = s[i];
-            s[i] = s[j];
-            s[j] = temp;
-            i++;
-            j--;
-        }        
+        while(left < right){
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+            left++;
+            right--;
+        }
+        return;
     }
 }
