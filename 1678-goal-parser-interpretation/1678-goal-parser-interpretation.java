06f2386d02980fc71e4d3aba9977caf48ee5a20f
Sonu Soni <1by23cb053@bmsit.in>
2026-08-22 23:47:05 +0530

    Time: 1 ms (74.4%), Space: 43.1 MB (17.03%) - LeetHub

--- 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
@@ -2,14 +2,10 @@ class Solution {
     public String interpret(String command) {
 
         StringBuilder sb = new StringBuilder();
-
         int i = 0;
         while(i < command.length()){
-
-            char ch = command.charAt(i);
-
-            if(ch == 'G'){
-                sb.append(ch);
+            if(command.charAt(i) == 'G'){
+                sb.append("G");
                 i++;
             }
             else if(command.charAt(i+1) == ')'){
