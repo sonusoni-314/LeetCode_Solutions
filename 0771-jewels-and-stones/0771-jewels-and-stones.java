c6c24f0130e1f29810650d4c643984dca0a3eb3f
Sonu Soni <1by23cb053@bmsit.in>
2026-08-23 00:37:37 +0530

    Time: 1 ms (61.69%), Space: 43.3 MB (36.59%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.java
+++ 0771-jewels-and-stones/0771-jewels-and-stones.java
@@ -1,15 +1,18 @@
 class Solution {
     public int numJewelsInStones(String jewels, String stones) {
+
+        HashSet <Character> set = new HashSet <>();
         int count = 0;
 
-        for (int i = 0; i < stones.length(); i++) {
-            for (int j = 0; j < jewels.length(); j++) {
-                if (stones.charAt(i) == jewels.charAt(j)) {
-                    count++;
-                }
-            }
+        for(char ch : jewels.toCharArray()){
+            set.add(ch);
         }
 
+        for(char ch : stones.toCharArray()){
+            if(set.contains(ch)){
+                count++;
+            }
+        }
         return count;
     }
 }
