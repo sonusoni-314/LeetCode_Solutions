73d0ea2a8806d8be8440ea59df207cd676dbecab
Sonu Soni <1by23cb053@bmsit.in>
2026-08-22 18:21:17 +0530

    Time: 0 ms (100%), Space: 42.6 MB (77.48%) - LeetHub

--- 1108-defanging-an-ip-address/1108-defanging-an-ip-address.java
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.java
@@ -1,17 +1,7 @@
 class Solution {
     public String defangIPaddr(String address) {
 
-        StringBuilder ans = new StringBuilder();
-
-        for(char ch : address.toCharArray()){
-            if(ch == '.'){
-                ans.append("[.]");
-            }else{
-                ans.append(ch);
-            }
-        }
-        return ans.toString();
+        return address.replace(".", "[.]");
+        
     }
-
-    // return address.replace(".", "[.]");
 }
