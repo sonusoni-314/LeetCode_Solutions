30e2d32cc26f2b61b7460f5cb241efb662e5cae2
Sonu Soni <1by23cb053@bmsit.in>
2026-08-23 00:21:10 +0530

    Time: 1 ms (99.41%), Space: 42.8 MB (74.12%) - LeetHub

--- 2315-count-asterisks/2315-count-asterisks.java
+++ 2315-count-asterisks/2315-count-asterisks.java
@@ -1,12 +1,10 @@
 class Solution {
     public int countAsterisks(String s) {
 
-        int count = 0;
         boolean inside = false;
+        int count = 0;
 
-        for(int i=0; i<s.length(); i++){
-            char ch = s.charAt(i);
-
+        for(char ch : s.toCharArray()){
             if(ch == '|'){
                 inside = !inside;
             }
