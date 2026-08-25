2c21dc7f34d620d25cd6cdbca6ca6a699a55a2b0
Sonu Soni <1by23cb053@bmsit.in>
2026-08-25 19:28:06 +0530

    Time: 2 ms (68.2%), Space: 45.5 MB (38.11%) - LeetHub

---
+++ 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int missingMultiple(int[] nums, int k) {
+
+        HashSet <Integer> set = new HashSet <>();
+
+        for(int x : nums){
+            set.add(x);
+        }
+
+        int multiple = k;
+
+        while(multiple <= 100){
+            
+            if(set.contains(multiple)){
+                multiple += k;
+            }else{
+                break;
+            }            
+        }
+        return multiple;        
+    }
+}
