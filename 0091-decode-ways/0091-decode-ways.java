6551af6c4e210fb9aea6e60e5133bd501cf6556b
Sonu Soni <1by23cb053@bmsit.in>
2026-08-31 13:10:00 +0530

    Time: 0 ms (100%), Space: 43.1 MB (48.52%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -1,63 +1,32 @@
-// tabulation:
-
 class Solution {
     public int numDecodings(String s) {
 
+        int[] memo = new int[s.length() + 1];
+        Arrays.fill(memo, -1);
+        return solve(0, s, memo);
+        
+    }
 
-        int n = s.length();
-        int[] dp = new int[n+1];
-        dp[n] = 1;
+    public int solve(int i, String s, int[] memo){
+        if(i == s.length()){
+            return 1;
+        }
 
-        for(int i=n-1; i>=0; i--){
-            if(s.charAt(i) == '0'){
-                dp[i] = 0;
-            }else{
-                dp[i] = dp[i+1];
+        if(s.charAt(i) == '0'){
+            return 0;
+        }
 
-                if(i+1 < n){
-                    if(s.charAt(i) == '1' || s.charAt(i) == '2' && s.charAt(i+1) <= '6'){
-                        dp[i] += dp[i+2];
-                    }
-                }
+        if(memo[i] != -1)   return memo[i];
+
+        int take1 = solve(i+1, s, memo);
+        int take2 = 0;
+
+        if(i+1 < s.length()){
+            if(s.charAt(i) == '1' || s.charAt(i) == '2' && s.charAt(i+1) <= '6'){
+                take2 = solve(i+2, s, memo);
             }
         }
-        return dp[0];        
+
+        return memo[i] = take1 + take2;
     }
 }
-
-
-// Memoi code
-
-// class Solution {
-//     public int numDecodings(String s) {
-
-//         int[] dp = new int[s.length() + 1];
-//         Arrays.fill(dp, -1);
-//         return solve(0, s, dp);
-        
-//     }
-
-//     public int solve(int i, String s, int[] dp){
-//         if(i == s.length()){
-//             return 1;
-//         }
-
-//         if(s.charAt(i) == '0'){
-//             return 0;
-//         }
-
-//         if(dp[i] != -1) return dp[i];
-
-//         int take1 = solve(i+1, s, dp);
-
-//         int take2 = 0;
-
-//         if(i+1 < s.length()){
-//             if(s.charAt(i)=='1' || s.charAt(i)=='2' && s.charAt(i+1)<='6'){
-//                 take2 = solve(i+2, s, dp);
-//             }
-//         }
-
-//         return dp[i] = take1 + take2;
-//     }
-// }
