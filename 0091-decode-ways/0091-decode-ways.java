6434c87c4b70339ffe721b7b3b3a23773aa2db2d
Sonu Soni <1by23cb053@bmsit.in>
2026-08-31 13:33:22 +0530

    Time: 0 ms (100%), Space: 42.7 MB (93.61%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -1,19 +1,18 @@
-// tabulation:
-
 class Solution {
     public int numDecodings(String s) {
 
-
         int n = s.length();
+
         int[] dp = new int[n+1];
+
         dp[n] = 1;
 
         for(int i=n-1; i>=0; i--){
             if(s.charAt(i) == '0'){
                 dp[i] = 0;
-            }else{
-                dp[i] = dp[i+1];
-
+            }
+            else{
+                dp[i] += dp[i+1];
                 if(i+1 < n){
                     if(s.charAt(i) == '1' || s.charAt(i) == '2' && s.charAt(i+1) <= '6'){
                         dp[i] += dp[i+2];
@@ -21,23 +20,20 @@ class Solution {
                 }
             }
         }
-        return dp[0];        
+        return dp[0];
     }
 }
-
-
-// Memoi code
 
 // class Solution {
 //     public int numDecodings(String s) {
 
-//         int[] dp = new int[s.length() + 1];
-//         Arrays.fill(dp, -1);
-//         return solve(0, s, dp);
+//         int[] memo = new int[s.length() + 1];
+//         Arrays.fill(memo, -1);
+//         return solve(0, s, memo);
         
 //     }
 
-//     public int solve(int i, String s, int[] dp){
+//     public int solve(int i, String s, int[] memo){
 //         if(i == s.length()){
 //             return 1;
 //         }
@@ -46,18 +42,17 @@ class Solution {
 //             return 0;
 //         }
 
-//         if(dp[i] != -1) return dp[i];
+//         if(memo[i] != -1)   return memo[i];
 
-//         int take1 = solve(i+1, s, dp);
-
+//         int take1 = solve(i+1, s, memo);
 //         int take2 = 0;
 
 //         if(i+1 < s.length()){
-//             if(s.charAt(i)=='1' || s.charAt(i)=='2' && s.charAt(i+1)<='6'){
-//                 take2 = solve(i+2, s, dp);
+//             if(s.charAt(i) == '1' || s.charAt(i) == '2' && s.charAt(i+1) <= '6'){
+//                 take2 = solve(i+2, s, memo);
 //             }
 //         }
 
-//         return dp[i] = take1 + take2;
+//         return memo[i] = take1 + take2;
 //     }
 // }
