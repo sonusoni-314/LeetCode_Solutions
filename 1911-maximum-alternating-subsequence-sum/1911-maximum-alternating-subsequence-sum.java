c6d555add615a6122d37d153d06a6d45865de530
Sonu Soni <1by23cb053@bmsit.in>
2026-08-31 11:31:01 +0530

    Time: 61 ms (36.85%), Space: 122 MB (31.67%) - LeetHub

--- 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.java
+++ 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.java
@@ -1,21 +1,26 @@
 class Solution {
     public long maxAlternatingSum(int[] nums) {
-        long[][] dp = new long[nums.length+1][2];
-        for(int i=0; i<nums.length; i++){
+
+        int n = nums.length;
+
+        long[][] dp = new long[n+1][2];
+        for(int i=0; i<n; i++){
             Arrays.fill(dp[i], -1);
         }
         return solve(0, 1, nums, dp);        
     }
 
     public long solve(int i, int flag, int[] nums, long[][] dp){
-        if (i == nums.length)   return 0;
+        if(i == nums.length){
+            return 0;
+        }
 
         if(dp[i][flag] != -1){
             return dp[i][flag];
         }
 
         long skip = solve(i+1, flag, nums, dp);
-        long take;
+        long take = 0;
 
         if(flag == 1){
             take = nums[i] + solve(i+1, 0, nums, dp);
